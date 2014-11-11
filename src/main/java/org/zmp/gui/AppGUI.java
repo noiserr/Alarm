48952a298d3b7b276074a71299d662c1b3703c0d
noiserr <gi#mf225nntwk$>
2014-11-11 12:59:30 +0100

    2

---
+++ src/main/java/org/zmp/gui/AppGUI.java
@@ -0,0 +1,17 @@
+package org.zmp.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by MM on 2014-11-11.hhh
+ */
+public class AppGUI {
+    public AppGUI(){
+        JFrame frame = new JFrame("Alarm");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(new Dimension(300,400));
+        frame.setVisible(true);
+        frame.setVisible(true);
+    }
+}
